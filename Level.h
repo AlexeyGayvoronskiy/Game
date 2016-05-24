a05bfbee83dce1bbacf59a0bba96f65c5ed4d9db
AlexeyGayvoronskiy <alexey.gayvoronskiy@mail.ru>
2016-05-24 22:49:35 +0300

    Attaching

---
+++ Level.h
@@ -0,0 +1,23 @@
+#ifndef _LEVEL_H_
+#define _LEVEL_H_
+
+#include "Project.h"
+
+#include "Graphics.h"
+class Graphics;
+#include "Input.h"
+class Input;
+#include "Screen.h"
+class Screen;
+
+class Level
+{
+private:
+
+
+public:
+	Level();
+	~Level();
+};
+
+#endif
